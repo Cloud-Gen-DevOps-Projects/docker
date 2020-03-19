fadce1cddc5f6aaf11572129ff6b9c6cf1808208
root <root@localhost.localdomain>
2020-03-19 19:50:18 +0530

    adda.java v1

---
+++ addd.java
@@ -0,0 +1,14 @@
+class addd
+{
+		public static void main(String[] args)
+		{
+			int a,b,c;
+			a=50;
+			b=100;
+			c=230;
+			c=a+b+c;
+			System.out.println("The sum of three values are "+c);
+		}
+}
+
+
