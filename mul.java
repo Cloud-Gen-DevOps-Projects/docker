ebe620d39e82204aa8a20c64e6cd0f27b444d8b2
root <root@localhost.localdomain>
2020-03-19 20:05:48 +0530

    mul.java v1

---
+++ mul.java
@@ -0,0 +1,13 @@
+class mul
+{
+		public static void main(String[] args)
+		{
+			int a,b,c;
+			a=50;
+			b=100;
+			c=230;
+			c=(a+b)*c;
+			System.out.println("The sum of three values are "+c);
+		}
+}
+
